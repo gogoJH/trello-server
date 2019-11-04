d8342c2075b6dd11528aa8484891ea7bcfbf7443
gogoJH <wlsgur4033@gmail.com>
2019-11-05 03:17:57 +0900

    join table test2

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -1,8 +1,8 @@
 import express from "express";
-//import controller from "../controller/board";
+import controller from "../controller/board";
 
 const router = express.Router();
 
-router.get("/");
+router.get("/", controller.getCards);
 
 export = router;
